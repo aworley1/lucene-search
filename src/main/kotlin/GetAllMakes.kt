fefb492544b91c1b29f1122da66c3bad22d6dc75
Andrew Worley <andrew.worley@johnlewis.co.uk>
2023-05-17 14:00:13 +0100

    Add GetAllMakes function
    which just gets all the documents out then uses Kotlin to find the distinct ones

--- src/main/kotlin/GetAllMakes.kt
+++ src/main/kotlin/GetAllMakes.kt
@@ -1,19 +1,20 @@
 import org.apache.lucene.index.DirectoryReader
 import org.apache.lucene.index.Term
 import org.apache.lucene.search.IndexSearcher
+import org.apache.lucene.search.MatchAllDocsQuery
 import org.apache.lucene.search.PrefixQuery
 
 fun main() {
     val reader = DirectoryReader.open(Index.index)
     val searcher = IndexSearcher(reader)
 
-    val query = PrefixQuery(Term("model", "iphone"))
+    val query = MatchAllDocsQuery()
     val results = searcher.search(query, Int.MAX_VALUE)
 
     println(results.totalHits)
 
-    results.scoreDocs.forEach {
-        println(searcher.indexReader.storedFields().document(it.doc))
-    }
+    results.scoreDocs.map {
+        searcher.indexReader.storedFields().document(it.doc).getField("make").stringValue()
+    }.distinct().also { println(it) }
 
 }
